e240ed6fbe56d6d57499812fb79c4a6486e3acd0
Prasanna <gramopadhye37@gmail.com>
2022-05-21 14:12:25 +0530

    First commit

--- src/redux/actions/postAction.js
+++ src/redux/actions/postAction.js
@@ -13,14 +13,33 @@ const axios = require("axios");
 
 const host = "https://facebook-clone-react-new.herokuapp.com";
 
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
 export const sendPost = (caption) => async (dispatch) => {
   try {
     dispatch({
       type: REQUIRE_POST,
     });
 
+    let token = getCookie("authToken")
+
     const link = `${host}/api/v1/createPost`;
     const {data} = await axios.post(link, {
+      authToken: token,
       caption: caption,
       image: {
         public_id: "public_id",
@@ -47,8 +66,12 @@ export const getPost = () => async (dispatch) => {
             type: REQUIRE_GET_POST
         })
 
+        let token = getCookie("authToken")
+
         const link = `${host}/api/v1/getPosts`;
-        const {data} = await axios.get(link);
+        const {data} = await axios.post(link,{
+          authToken: token,
+        });
 
         dispatch({
             type: GET_POST_SUCCESS,
@@ -69,8 +92,13 @@ export const addComment = (postId, comment) => async (dispatch) => {
       type: REQUIRE_ADD_COMMENT
     })
 
+    let token = getCookie("authToken")
+
     const link = `${host}/api/v1/comment/${postId}`
-    const {data} = await axios.post(link, {comment: comment});
+    const {data} = await axios.post(link, {
+      authToken: token,
+      comment: comment
+    });
 
     dispatch({
       type: ADD_COMMENT_SUCCESS,
--- src/redux/actions/userAction.js
+++ src/redux/actions/userAction.js
@@ -11,6 +11,22 @@ const axios = require("axios");
 const host = "https://facebook-clone-react-new.herokuapp.com";
 // const host = "http://localhost:5000";
 
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
 const setCookie = (cname, cvalue, exdays) => {
   const d = new Date();
   d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
@@ -119,8 +135,13 @@ export const loadUser = () => async (dispatch) => {
       type: "REQUIRE_USER"
     })
 
+    let token = getCookie("authToken")
+
     const link = `${host}/api/v1/myProfile`;
-    const {data} = await axios.get(link);
+
+    const {data} = await axios.post(link,{
+      authToken: token
+    });
     console.log(data)
     dispatch({
       type: "USER_SUCCESS",
